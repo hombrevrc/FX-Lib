48abd833de2b86d89fedf7574dd47b9530822909
marmysh <marmysh@gmail.com>
2016-02-28 11:19:49 +0300

    New project - Threading.

--- Threading/stdafx.h
+++ Threading/stdafx.h
@@ -3,24 +3,32 @@
 //==============================================================
 
 #pragma once
+
+#include "../System/Sal.h"
+
+#ifdef FX_LIB_WINDOWS
 #ifdef _DEBUG
 #define _CRTDBG_MAP_ALLOC
 #endif
+#endif
 
+#ifdef FX_LIB_WINDOWS
 #include "targetver.h"
-
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-
 #include <Windows.h>
+#include <atlbase.h>
+
 #pragma warning (disable : 4091) // 'typedef ': ignored on left of '' when no variable is declared
 #pragma warning (push)
 #include <DbgHelp.h>
 #pragma warning (pop)
+
+#endif
+
 #include <stdint.h>
 #include <assert.h>
 #include <vector>
 #include <string>
-#include <process.h>
 #include <memory>
 #include <atomic>
 #include <thread>
@@ -30,13 +38,15 @@
 #include <unordered_map>
 #include <sstream>
 #include <mutex>
+#include <condition_variable>
 
+#ifdef FX_LIB_WINDOWS
 #ifdef _DEBUG
 #include <crtdbg.h>
 #define new new (_CLIENT_BLOCK, __FILE__, __LINE__)
 #endif
+#endif
 
-#include "../System/System.h"
 
 #ifdef max
 #undef max
