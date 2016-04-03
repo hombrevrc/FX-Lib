15e2326db5c633242b8f3ff645c82f2f169fb177
Viktar Marmysh <marmysh@gmail.com>
2016-04-03 22:53:06 +0300

    ReplaceAll function.

--- System/FileSystem.Linux.hpp
+++ System/FileSystem.Linux.hpp
@@ -7,8 +7,8 @@ std::filesystem::path GetStartupDirectory()
 	//init executable url
 	char buffer[PATH_MAX] = "";
 	sprintf(buffer, "/proc/%d/exe", getpid());
-    std::string st = buffer;
-    readlink(st.c_str(), buffer, sizeof(buffer));
+	std::string st = buffer;
+	readlink(st.c_str(), buffer, sizeof(buffer));
 	std::filesystem::path result = buffer;
 	result = result.parent_path();
 	return result;
--- System/Functions.h
+++ System/Functions.h
@@ -6,3 +6,15 @@
 
 std::tstring GenerateGuid();
 
+template<typename T>
+void ReplaceAll(const std::basic_string<T>& oldValue, const std::basic_string<T>& newValue, std::basic_string<T>& text)
+{
+	if (!oldValue.empty())
+	{
+		for (size_t pos = text.find(oldValue, 0); std::string::npos != pos; pos = text.find(oldValue, pos))
+		{
+			text.replace(pos, oldValue.length(), newValue);
+			pos += newValue.length();
+		}
+	}
+}
